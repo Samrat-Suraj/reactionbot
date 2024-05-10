54d66536513f73ad33fd852608b48e5b274395ea
shreerambhakthhu <167292251+shreerambhakthhu@users.noreply.github.com>
2024-05-10 21:35:15 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,43 @@
+from pyrogram import Client, filters
+import requests
+import random
+
+api_id = 26480985 #--Add your Api Id here
+api_hash = '56c935fae1c5c86ba5a3af655f8caa9d' #--Enter Api Hash Here
+
+token = 7195594057:AAHfeGa1M11mAVFOo8SfUDYPZG-vIAx8_Ow' #--Enter Bot Token Here.
+
+emojis = ["👍", "👎", "❤️", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊", "🤡", "🥱", "🥴", "😍", "🐳", "❤‍🔥", "🌚", "🌭", "💯", "🤣", "⚡️", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋", "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍️", "🤗", "🫡", "🎅", "🎄", "☃️", "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂", "🤷", "🤷‍♀", "😡"]
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token)
+
+@app.on_message()
+async def react_to_message(client, message):
+    chat_id = message.chat.id
+    message_id = message.id
+    
+    # Choose a random emoji from the list
+    random_emoji = random.choice(emojis)
+    
+    url = f'https://api.telegram.org/bot{token}/setMessageReaction'
+
+    # Parameters for the request
+    params = {
+        'chat_id': chat_id,
+        'message_id': message_id,
+        'reaction': [{
+            "type": "emoji",
+            "emoji": random_emoji
+        }]
+    }
+
+    response = requests.post(url, json=params)
+
+    if response.status_code == 200:
+        print("Reaction set successfully!")
+        print("Response content:", response.content)
+    else:
+        print(f"Failed to set reaction. Status code: {response.status_code}")
+        print("Response content:", response.content)
+    
+app.run()

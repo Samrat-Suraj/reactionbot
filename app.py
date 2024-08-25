2fae8981044889e6308097e5bf0b3056637aa083
Samrat-Suraj <143329360+Samrat-Suraj@users.noreply.github.com>
2024-08-25 10:25:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the homepage
 @app.route('/')
 def hello_world():
-    return 'This bot is made by @sentrien and currently it hosted and live for everyone'
+    return 'This bot is made by @Devilss_God and currently it hosted and live for everyone'
 
 # Run the application
 if __name__ == '__main__':
